f3dc324eb440bd6611fc6517ed37c6255de98e08
SanathRai <sanathrai03@gmail.com>
2025-09-07 13:30:04 +0530

    Cors added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('../src/routes/auth.route.js')
 const foodRoutes = require('../src/routes/food.route.js')
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(cookieParser())
 app.use(express.json());
 
