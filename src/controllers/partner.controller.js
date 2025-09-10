5bf977265cd5f80d42406d62d13f091a79d04744
SanathRai <sanathrai03@gmail.com>
2025-09-10 09:14:09 +0530

    Partner pofile video fetched by id

--- src/controllers/partner.controller.js
+++ src/controllers/partner.controller.js
@@ -1,15 +1,21 @@
 const foodPartnerModel = require('../models/partner.model.js');
+const foodModel = require('../models/food.model.js');
 
 const getFoodPartnerById = async (req, res) => {
     try {
         const foodPartnerId = req.params.id;
         const foodPartner = await foodPartnerModel.findById(foodPartnerId);
+        const foodItemsByFoodPartner = await foodModel.find({ foodPartner: foodPartnerId });
+
         if (!foodPartner) {
             return res.status(404).json({ message: 'Food partner not found' });
         }
         res.status(200).json({
             message: "Food partner retrieved successfully",
-            foodPartner
+            foodPartner: {
+                ...foodPartner.toObject(),
+                foodItems: foodItemsByFoodPartner
+            }
         });
     } catch (error) {
         console.error('Error fetching food partner by ID:', error);
