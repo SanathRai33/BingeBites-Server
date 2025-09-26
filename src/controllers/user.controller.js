8197c7f02f1e3b8fd82e74cbe3747d6b9d028299
SanathRai <sanathrai03@gmail.com>
2025-09-26 22:54:23 +0530

    Developed user profile update controller and Router

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const userModel = require("../models/user.model.js");
 
 async function userProfile(req, res) {
   try {
@@ -20,7 +21,38 @@ async function userProfile(req, res) {
   }
 }
 
+async function updateUserProfile(req, res) {
+  try {
+    const { fullName, phone, address } = req.body;
+
+    const user = req.user._id;
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const userId = req.params.id;
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      { fullName, phone, address },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong", error });
+  }
+}
 
 module.exports = {
   userProfile,
+  updateUserProfile
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const { userProfile } = require('../controllers/user.controller.js')
+const { userProfile, updateUserProfile } = require('../controllers/user.controller.js')
 const { authUserMiddleware } = require('../middlewares/auth.middleware.js');
 
 router.get("/profile", authUserMiddleware, userProfile)
+router.put('/profile', authUserMiddleware, updateUserProfile)
 
 module.exports = router;
