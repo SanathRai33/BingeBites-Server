fb319876013bd480bfd9ff75ac759313c35966b2
SanathRai <sanathrai03@gmail.com>
2025-09-26 23:02:08 +0530

    Updated/UserModel(Phone and Address added)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,22 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      // required: [true, "Phone number is required"],
+      match: [/^\d{10}$/, "Phone number must be exactly 10 digits"],
+      unique: true,
+    },
+    address: {
+      type: String,
+      // required: [true, "Address is required"],
+      minlength: [10, "Address must be at least 10 characters long"],
+      maxlength: [200, "Address cannot exceed 200 characters"],
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-},
-{
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("user", userSchema);
 
