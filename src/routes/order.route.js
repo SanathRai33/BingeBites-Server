1deb196a060e9ed44fd9f91e5a1362a37e26a00b
SanathRai <sanathrai03@gmail.com>
2025-10-05 23:07:46 +0530

    Debug/OrderRoute (func not called)

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authUserMiddleware, authFoodPartnerMiddleware } = require('../middlewares/auth.middleware.js');
-const { placeOrder } = require('../controllers/order.controller.js');
+const { placeOrder, getOrdersById } = require('../controllers/order.controller.js');
 
 router.post('/', authUserMiddleware, placeOrder);
 router.get('/', authFoodPartnerMiddleware, getOrdersById);
